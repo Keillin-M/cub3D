600ce77898ddaf52c37585f6dbaefa7ac0475015
Thanakorn Thajan <tthajan@c4b1c1.42berlin.de>
2025-09-29 10:12:08 +0200

    removed duplicate and redundant

--- src/render.c
+++ src/render.c
@@ -6,13 +6,18 @@
 /*   By: tthajan <tthajan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 00:00:00 by kmaeda            #+#    #+#             */
-/*   Updated: 2025/09/29 10:03:20 by tthajan          ###   ########.fr       */
+/*   Updated: 2025/09/29 10:11:38 by tthajan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
 #include <math.h>
+
+// Window and render constants
+#define WIN_WIDTH 1920
+#define WIN_HEIGHT 1080
+#define RENDER_STEP 2    // Render every 2nd pixel for performance
 
 // Forward declarations for internal functions
 void	render_walls_raycast(t_game *game, char *img_data, int line_len);
@@ -47,13 +52,13 @@ int create_window(t_game *game)
         return (0);
     }
 
-    game->win = mlx_new_window(game->mlx, 800, 600, "cub3D");
+    game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, "cub3D");
     if (!game->win)
     {
         printf("Error: Failed to create window\n");
         return (0);
     }
-    printf("Window created: 800x600 - cub3D\n");
+    printf("Window created: %dx%d - cub3D\n", WIN_WIDTH, WIN_HEIGHT);
     return (1);
 }
 
@@ -97,7 +102,7 @@ void	put_pixel_to_img(char *img_data, int x, int y, int color, int line_len)
 {
 	int	pixel_index;
 	
-	if (x >= 0 && x < 1920 && y >= 0 && y < 1080)
+	if (x >= 0 && x < WIN_WIDTH && y >= 0 && y < WIN_HEIGHT)
 	{
 		pixel_index = (y * line_len) + (x * 4);
 		img_data[pixel_index] = color & 0xFF;           // Blue
@@ -107,113 +112,7 @@ void	put_pixel_to_img(char *img_data, int x, int y, int color, int line_len)
 	}
 }
 
-/**
- * Cast a single ray using the proper ray structure
- * @param game: Game structure with map and player data  
- * @param x: Screen column to cast ray for
- */
-void	cast_single_ray(t_game *game, int x)
-{
-	t_ray	ray;
-	
-	// Initialize ray for this screen column
-	ray.camera_x = 2 * x / (double)1920 - 1;
-	ray.ray_dir_x = game->player.dir_x + game->player.plane_x * ray.camera_x;
-	ray.ray_dir_y = game->player.dir_y + game->player.plane_y * ray.camera_x;
-	
-	// Current position
-	ray.map_x = (int)game->player.x;
-	ray.map_y = (int)game->player.y;
-	
-	// Calculate delta distances
-	ray.delta_dist_x = (ray.ray_dir_x == 0) ? 1e30 : fabs(1 / ray.ray_dir_x);
-	ray.delta_dist_y = (ray.ray_dir_y == 0) ? 1e30 : fabs(1 / ray.ray_dir_y);
-	
-	// Calculate step and initial side_dist
-	if (ray.ray_dir_x < 0)
-	{
-		ray.step_x = -1;
-		ray.side_dist_x = (game->player.x - ray.map_x) * ray.delta_dist_x;
-	}
-	else
-	{
-		ray.step_x = 1;
-		ray.side_dist_x = (ray.map_x + 1.0 - game->player.x) * ray.delta_dist_x;
-	}
-	
-	if (ray.ray_dir_y < 0)
-	{
-		ray.step_y = -1;
-		ray.side_dist_y = (game->player.y - ray.map_y) * ray.delta_dist_y;
-	}
-	else
-	{
-		ray.step_y = 1;
-		ray.side_dist_y = (ray.map_y + 1.0 - game->player.y) * ray.delta_dist_y;
-	}
-	
-	// Perform DDA
-	ray.hit = 0;
-	while (ray.hit == 0)
-	{
-		if (ray.side_dist_x < ray.side_dist_y)
-		{
-			ray.side_dist_x += ray.delta_dist_x;
-			ray.map_x += ray.step_x;
-			ray.side = 0;
-		}
-		else
-		{
-			ray.side_dist_y += ray.delta_dist_y;
-			ray.map_y += ray.step_y;
-			ray.side = 1;
-		}
-		
-		// Check if ray has hit a wall
-		if (ray.map_x < 0 || ray.map_x >= game->map->width || 
-			ray.map_y < 0 || ray.map_y >= game->map->height ||
-			game->map->map[ray.map_y][ray.map_x] == '1')
-		{
-			ray.hit = 1;
-		}
-	}
-	
-	// Calculate distance
-	if (ray.side == 0)
-		ray.perp_wall_dist = (ray.map_x - game->player.x + (1 - ray.step_x) / 2) / ray.ray_dir_x;
-	else
-		ray.perp_wall_dist = (ray.map_y - game->player.y + (1 - ray.step_y) / 2) / ray.ray_dir_y;
-	
-	// Calculate line height
-	ray.line_height = (int)(1080 / ray.perp_wall_dist);
-	
-	// Calculate draw start and end
-	ray.draw_start = -ray.line_height / 2 + 1080 / 2;
-	if (ray.draw_start < 0)
-		ray.draw_start = 0;
-	ray.draw_end = ray.line_height / 2 + 1080 / 2;
-	if (ray.draw_end >= 1080)
-		ray.draw_end = 1080 - 1;
-	
-	// For now, we need access to the image buffer to draw directly
-	// TODO: Refactor this to use proper render structure
-	// This is a temporary solution to get basic raycasting working
-}
 
-/**
- * Cast rays for all screen columns using existing function signature
- * @param game: Game structure
- */
-void	cast_rays(t_game *game)
-{
-	int	x;
-	
-	// Cast a ray for each screen column (every 4th pixel for performance)
-	for (x = 0; x < 1920; x += 4)
-	{
-		cast_single_ray(game, x);
-	}
-}
 
 /**
  * Basic rendering function with integrated raycasting
@@ -228,18 +127,18 @@ int	render(t_game *game)
 	int		x, y;
 	
 	// Create image for better performance
-	img = mlx_new_image(game->mlx, 1920, 1080);
+	img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
 	if (!img)
 		return (1);
 		
 	img_data = mlx_get_data_addr(img, &bpp, &line_len, &endian);
 	
 	// Draw background - ceiling and floor
-	for (y = 0; y < 1080; y++)
+	for (y = 0; y < WIN_HEIGHT; y++)
 	{
-		for (x = 0; x < 1920; x++)
+		for (x = 0; x < WIN_WIDTH; x++)
 		{
-			if (y < 540)
+			if (y < WIN_HEIGHT / 2)
 				put_pixel_to_img(img_data, x, y, 0x87CEEB, line_len); // Ceiling
 			else
 				put_pixel_to_img(img_data, x, y, 0x8B4513, line_len); // Floor
@@ -268,11 +167,11 @@ void	render_walls_raycast(t_game *game, char *img_data, int line_len)
 	t_ray	ray;
 	int		color;
 	
-	// Cast a ray for each screen column (every 2nd pixel for performance)  
-	for (x = 0; x < 1920; x += 2)
+	// Cast a ray for each screen column (every nth pixel for performance)  
+	for (x = 0; x < WIN_WIDTH; x += RENDER_STEP)
 	{
 		// Initialize ray
-		ray.camera_x = 2 * x / (double)1920 - 1;
+		ray.camera_x = 2 * x / (double)WIN_WIDTH - 1;
 		ray.ray_dir_x = game->player.dir_x + game->player.plane_x * ray.camera_x;
 		ray.ray_dir_y = game->player.dir_y + game->player.plane_y * ray.camera_x;
 		
@@ -340,15 +239,15 @@ void	render_walls_raycast(t_game *game, char *img_data, int line_len)
 			ray.perp_wall_dist = (ray.map_y - game->player.y + (1 - ray.step_y) / 2) / ray.ray_dir_y;
 		
 		// Calculate line height
-		ray.line_height = (int)(1080 / ray.perp_wall_dist);
+		ray.line_height = (int)(WIN_HEIGHT / ray.perp_wall_dist);
 		
 		// Calculate draw start and end
-		ray.draw_start = -ray.line_height / 2 + 1080 / 2;
+		ray.draw_start = -ray.line_height / 2 + WIN_HEIGHT / 2;
 		if (ray.draw_start < 0)
 			ray.draw_start = 0;
-		ray.draw_end = ray.line_height / 2 + 1080 / 2;
-		if (ray.draw_end >= 1080)
-			ray.draw_end = 1080 - 1;
+		ray.draw_end = ray.line_height / 2 + WIN_HEIGHT / 2;
+		if (ray.draw_end >= WIN_HEIGHT)
+			ray.draw_end = WIN_HEIGHT - 1;
 		
 		// Choose wall color - different colors for different sides and distance
 		if (ray.side == 0)
@@ -362,11 +261,12 @@ void	render_walls_raycast(t_game *game, char *img_data, int line_len)
 		else if (ray.perp_wall_dist > 4)
 			color = color >> 1; // Darker for far walls
 		
-		// Draw the wall stripe  
+		// Draw the wall stripe (fill RENDER_STEP pixels to avoid gaps)
 		for (y = ray.draw_start; y < ray.draw_end; y++)
 		{
-			put_pixel_to_img(img_data, x, y, color, line_len);
-			put_pixel_to_img(img_data, x + 1, y, color, line_len); // Fill the gap
+			int i;
+			for (i = 0; i < RENDER_STEP && (x + i) < WIN_WIDTH; i++)
+				put_pixel_to_img(img_data, x + i, y, color, line_len);
 		}
 	}
 }
